1e3856bfde4644d5c9815919894ee666923e0ed6
dy <df.creative@gmail.com>
2018-09-24 14:25:35 +0300

    Lintify

--- index.js
+++ index.js
@@ -13,25 +13,25 @@ module.exports = function flip (pixels, w, h, c) {
 	if (!w || !h) throw Error('Bad dimensions')
 	if (!c) c = pixels.length / (w * h)
 
-	var h2 = h / 2 | 0;  // the | 0 keeps the result an int
-	var row = w * c;
+	var h2 = h >> 1
+	var row = w * c
 	var Ctor = pixels.constructor
 
 
 	// make a temp buffer to hold one row
-	var temp = new Ctor(w * c);
+	var temp = new Ctor(w * c)
 	for (var y = 0; y < h2; ++y) {
-	  var topOffset = y * row;
-	  var bottomOffset = (h - y - 1) * row;
+		var topOffset = y * row
+		var bottomOffset = (h - y - 1) * row
 
-	  // make copy of a row on the top half
-	  temp.set(pixels.subarray(topOffset, topOffset + row));
+		// make copy of a row on the top half
+		temp.set(pixels.subarray(topOffset, topOffset + row))
 
-	  // copy a row from the bottom half to the top
-	  pixels.copyWithin(topOffset, bottomOffset, bottomOffset + row);
+		// copy a row from the bottom half to the top
+		pixels.copyWithin(topOffset, bottomOffset, bottomOffset + row)
 
-	  // copy the copy of the top half row to the bottom half
-	  pixels.set(temp, bottomOffset);
+		// copy the copy of the top half row to the bottom half
+		pixels.set(temp, bottomOffset)
 	}
 
 	return pixels
